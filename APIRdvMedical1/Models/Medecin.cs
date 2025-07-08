87cdcfc8b52194fdc89c6b7cb7771702b584c709
mamadou gueye <odcmamadougueye@gmail.com>
2025-07-09 00:18:58 +0200

    Initialisation du projet ASP.NET Web API pour la gestion de RDV médicaux

--- APIRdvMedical1/Models/Medecin.cs
+++ APIRdvMedical1/Models/Medecin.cs
@@ -2,24 +2,38 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace APIRdvMedical1.Model
-{ 
+{
+    /// <summary>
+    /// Représente un médecin, héritant de la classe Utilisateur.
+    /// </summary>
     public class Medecin : Utilisateur
     {
-        //[MaxLength(100)]
-        //public string Specialite { get; set; }
+        /// <summary>
+        /// Identifiant de la spécialité associée au médecin.
+        /// Cette propriété est la clé étrangère vers la table Specialite.
+        /// </summary>
         public int IdSpecialite { get; set; }
 
+        /// <summary>
+        /// Objet navigationnel représentant la spécialité médicale du médecin.
+        /// Chargé automatiquement via Entity Framework grâce à la clé étrangère.
+        /// </summary>
         [ForeignKey("IdSpecialite")]
         public Specialite Specialite { get; set; }
 
+        /// <summary>
+        /// Numéro d'inscription à l'ordre des médecins.
+        /// Limité à 10 caractères maximum.
+        /// </summary>
         [MaxLength(10)]
         public string NumeroOrdre { get; set; }
-        public virtual ICollection<Agenda> agenda { get; set; }
 
+        /// <summary>
+        /// Liste des créneaux d’agenda associés au médecin.
+        /// Un médecin peut avoir plusieurs agendas (rendez-vous).
+        /// </summary>
+        public virtual ICollection<Agenda> agenda { get; set; }
     }
 }
