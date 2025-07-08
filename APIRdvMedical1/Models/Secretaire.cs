9185d1b7a9138e361717403af7a296913ecb8d57
mamadou gueye <odcmamadougueye@gmail.com>
2025-07-09 00:30:04 +0200

    dSecraitaire

--- APIRdvMedical1/Models/Secretaire.cs
+++ APIRdvMedical1/Models/Secretaire.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace APIRdvMedical1.Model
 {
+    /// <summary>
+    /// Représente une secrétaire dans le système médical.
+    /// Hérite des propriétés de la classe Utilisateur.
+    /// </summary>
     public class Secretaire : Utilisateur
     {
+        /// <summary>
+        /// Numéro de téléphone fixe de la secrétaire.
+        /// Limité à 15 caractères.
+        /// </summary>
         [MaxLength(15)]
         public string TelephoneFixe { get; set; }
-
     }
 }
