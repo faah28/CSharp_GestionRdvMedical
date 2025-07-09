778b8273dfe6f38d8ce7d5900fa3dea7ab8afed3
mamadou gueye <odcmamadougueye@gmail.com>
2025-07-09 11:21:04 +0200

    Temp: sauvegarde avant rebase

--- MetierRvMedical/Properties/AssemblyInfo.cs
+++ MetierRvMedical/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly, changez les valeurs de ces attributs.
